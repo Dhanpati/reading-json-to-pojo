f7451569806ce8c9a9759f1d06e6be8fcdf1cb1d
DiPi <dhanpatineupane@gmail.com>
2020-04-11 13:10:56 +0200

    first commit

---
+++ target/classes/static/com/dipi/jackson/json/demo/Driver.java
@@ -0,0 +1,52 @@
+package com.dipi.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+	
+
+	public static void main(String[] args) {
+		try {
+			//Create object mapper 
+			ObjectMapper mapper=new ObjectMapper();
+			
+			
+			//Read JSON file and map/convert to java POJO
+			//Student theStudent=mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			Student theStudent=mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+			//print the object properties
+		System.out.println("ID:"   +theStudent.getId());
+		System.out.println("FirstName:"   +theStudent.getFirstName());
+		System.out.println("FirstName:"   +theStudent.getLastName());
+		
+		
+		Address theAddress=theStudent.getAddress();
+		
+		System.out.println("\nAddress:\n City:"   +theAddress.getCity());
+		System.out.println("State:"   +theAddress.getState());
+		
+		
+		String[] theLanguages=theStudent.getLanguages();
+		System.out.println("Languages");
+		for(String myobjectisin:theLanguages) {
+			System.out.println(myobjectisin);
+		}
+
+		
+		
+
+
+		
+			
+
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	
+	}
+
+}
